f5d5bb9eebd1f9d397286b91eaf80eb431f86461
Gonzalo <goespin@alumnos.ubiobio.cl>
2017-06-12 14:30:56 -0300

    Indica santoral a usuario

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -9,50 +9,51 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
-document.addEventListener('deviceready', function(){
-  $('#sub').bind('click', recibe);
-  $('#ayer').html(localStorage.getItem('ayer'));
-  $('#hoy').html(localStorage.getItem('hoy'));
-  $('#maniana').html(localStorage.getItem('maniana'));
-  //$('#euro').html(localStorage.getItem('euro'));
-  //$('#dolar').html(localStorage.getItem('dolar'));
-  $('uf').html(localStorage.getItem('uf'));
-  $('ipc').html(localStorage.getItem('ipc'));
-  $('utm').html(localStorage.getItem('utm'));
-  $('imacec').html(localStorage.getItem('imacec'));
-  $('normal').html(localStorage.getItem('normal'));
-  $('normal_maniana').html(localStorage.getItem('normal_maniana'));
-  $('catalitico').html(localStorage.getItem('catalitico'));
+document.addEventListener('deviceready', function () {
+    $('#sub').bind('click', recibe);
+    $('#ayer').html(localStorage.getItem('ayer'));
+    $('#hoy').html(localStorage.getItem('hoy'));
+    $('#maniana').html(localStorage.getItem('maniana'));
+    //$('#euro').html(localStorage.getItem('euro'));
+    //$('#dolar').html(localStorage.getItem('dolar'));
+    $('uf').html(localStorage.getItem('uf'));
+    $('ipc').html(localStorage.getItem('ipc'));
+    $('utm').html(localStorage.getItem('utm'));
+    $('imacec').html(localStorage.getItem('imacec'));
+    $('normal').html(localStorage.getItem('normal'));
+    $('normal_maniana').html(localStorage.getItem('normal_maniana'));
+    $('catalitico').html(localStorage.getItem('catalitico'));
 
 }, false);
 
-function recibe(){
-      myApp.showPreloader('Recibiendo datos...');
-      $.ajax({
-          dataType: 'json',
-          type: 'POST',
-          url: 'http://indicadoresdeldia.cl/webservice/indicadores.json',
-          success: function (data, status, xhr) {
-                  localStorage.setItem('ayer', data.santoral.ayer);
-		          localStorage.setItem('hoy', data.santoral.hoy);
-		          localStorage.setItem('maniana', data.santoral.maniana);
-		          //localStorage.setItem('dolar', data.moneda.dolar);
-		          //localStorage.setItem('euro', data.moneda.euro);
-		          localStorage.setItem('uf', data.indicador.uf);
-                  localStorage.setItem('ipc', data.indicador.ipc);
-		          localStorage.setItem('utm', data.indicador.utm);
-		          localStorage.setItem('imacec', data.indicador.imacec);
-                  localStorage.setItem('normal', data.restriccion.normal[0]);
-                  localStorage.setItem('normal_maniana', data.restriccion.normal_maniana[0]);
-                  localStorage.setItem('catalitico', data.restriccion.catalitico[0]);
-                  myApp.hidePreloader();
-                  myApp.alert("Mensaje <3",'Inicio de Aplición');
-                  window.location = "main.html";
-          },
-          error: function (xhr, status) {
-              myApp.hidePreloader();
-              myApp.alert('Hubo un error con la conexión','Error');
-          }
-      });
+function recibe() {
+    localStorage.setItem('nombre', document.getElementById('user').value);
+    myApp.showPreloader('Enviando datos...');
+    $.ajax({
+        dataType: 'json',
+        type: 'POST',
+        url: 'http://indicadoresdeldia.cl/webservice/indicadores.json',
+        success: function (data, status, xhr) {
+            localStorage.setItem('ayer', data.santoral.ayer);
+            localStorage.setItem('hoy', data.santoral.hoy);
+            localStorage.setItem('maniana', data.santoral.maniana);
+            //localStorage.setItem('dolar', data.moneda.dolar);
+            //localStorage.setItem('euro', data.moneda.euro);
+            localStorage.setItem('uf', data.indicador.uf);
+            localStorage.setItem('ipc', data.indicador.ipc);
+            localStorage.setItem('utm', data.indicador.utm);
+            localStorage.setItem('imacec', data.indicador.imacec);
+            localStorage.setItem('normal', data.restriccion.normal[0]);
+            localStorage.setItem('normal_maniana', data.restriccion.normal_maniana[0]);
+            localStorage.setItem('catalitico', data.restriccion.catalitico[0]);
+            myApp.hidePreloader();
+            myApp.alert("Mensaje <3", 'Inicio de Aplición');
+            window.location = "main.html";
+        },
+        error: function (xhr, status) {
+            myApp.hidePreloader();
+            myApp.alert('Hubo un error con la conexión', 'Error');
+        }
+    });
 }
 
--- platforms/browser/www/js/my-app2.js
+++ platforms/browser/www/js/my-app2.js
@@ -10,17 +10,42 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-document.addEventListener('deviceready', function(){
-  $('#ayer').html(localStorage.getItem('ayer'));
-  $('#hoy').html(localStorage.getItem('hoy'));
-  $('#maniana').html(localStorage.getItem('maniana'));
-  $('#uf').html(localStorage.getItem('uf'));
-  $('#ipc').html(localStorage.getItem('ipc'));
-  $('#utm').html(localStorage.getItem('utm'));
-  $('#imacec').html(localStorage.getItem('imacec'));
-  $('#normal').html(localStorage.getItem('normal'));
-  $('#normal_maniana').html(localStorage.getItem('normal_maniana'));
-  $('#catalitico').html(localStorage.getItem('catalitico'));
-  //$('#euro').html(localStorage.getItem('euro'));
-  //$('#dolar').html(localStorage.getItem('dolar'));
+document.addEventListener('deviceready', function () {
+    //$('#nombre').html(localStorage.getItem('nombre'));
+    $('#ayer').html(localStorage.getItem('ayer'));
+    $('#hoy').html(localStorage.getItem('hoy'));
+    $('#maniana').html(localStorage.getItem('maniana'));
+    $('#uf').html(localStorage.getItem('uf'));
+    $('#ipc').html(localStorage.getItem('ipc'));
+    $('#utm').html(localStorage.getItem('utm'));
+    $('#imacec').html(localStorage.getItem('imacec'));
+    $('#normal').html(localStorage.getItem('normal'));
+    $('#normal_maniana').html(localStorage.getItem('normal_maniana'));
+    $('#catalitico').html(localStorage.getItem('catalitico'));
+    //$('#euro').html(localStorage.getItem('euro'));
+    //$('#dolar').html(localStorage.getItem('dolar'));
+    var nombre = localStorage.getItem('nombre');
+    var ayer = localStorage.getItem('ayer');
+    var hoy = localStorage.getItem('hoy');
+    var maniana = localStorage.getItem('maniana');
+    ayer = ayer.split(',');
+    hoy = hoy.split(',');
+    maniana = maniana.split(',');
+    verificar();
+
+    function verificar() {
+        ayer.forEach(function (ayerNombre) {
+            if (nombre == ayerNombre.trim())
+                myApp.alert('Feliz santo atrasado ' + ayerNombre.trim() + ' <3 ', 'Felicidades !!!')
+        });
+        hoy.forEach(function (hoyNombre) {
+            if (nombre == hoyNombre.trim())
+                myApp.alert('Muy feliz santo ' + hoyNombre.trim() + ' <3 ', 'Felicidades !!!')
+        });
+        maniana.forEach(function (manianaNombre) {
+            if (nombre == manianaNombre.trim())
+                myApp.alert('Atento al día de tu santo mañana ' + manianaNombre.trim() + ' <3 ', 'Felicidades !!!')
+        });
+
+    }
 }, false);
